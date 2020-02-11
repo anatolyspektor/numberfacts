54fac6deead729a0b622eb4341e949a14a55be11
Anatoly Spektor <spektor.anatoly@gmail.com>
2020-02-11 17:57:14 +0000

    Wrap RestClient in exception

--- lib/numbers_api.rb
+++ lib/numbers_api.rb
@@ -2,14 +2,17 @@ require 'rest-client'
 
 module NumbersApi
 
+  # these uses RestClient to get a fact from api
   def self.get_fact(number)
-    response = RestClient.get("http://numbersapi.com/#{number}", headers={})
-    if response.code == 200
-      return response.body
-    else
-      return "Sorry pal, API is not responding!"
+    begin
+      response = RestClient.get("http://numbersttapi.com/#{number}", headers={})
+      if response.code == 200
+        return response.body
+      end
+        #this should probably more specific in future!
+    rescue Exception => err
+      return "Sorry pal, API is not responding today!"
     end
-
   end
 
 end
